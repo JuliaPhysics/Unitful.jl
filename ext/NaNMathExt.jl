89f4930d3e7063b9a02736557e164a55c0ffbd9a
Isaac Wheeler <47340776+Ickaser@users.noreply.github.com>
2025-12-05 21:02:48 -0500

    Broaden dispatch to AbstractQuantity
    Co-authored-by: Sebastian Stock <42280794+sostock@users.noreply.github.com>

--- ext/NaNMathExt.jl
+++ ext/NaNMathExt.jl
@@ -2,6 +2,6 @@ module NaNMathExt
 using Unitful
 import NaNMath
 
-NaNMath.sqrt(q::Quantity) = NaNMath.sqrt(ustrip(q))*sqrt(unit(q))
-NaNMath.pow(q::Quantity, r) = NaNMath.pow(ustrip(q), r)*unit(q)^r
+NaNMath.sqrt(q::Unitful.AbstractQuantity) = NaNMath.sqrt(ustrip(q))*sqrt(unit(q))
+NaNMath.pow(q::Unitful.AbstractQuantity, r) = NaNMath.pow(ustrip(q), r)*unit(q)^r
 end
